d614f4c09df67e08ead574947acb55a6f4fd9e31
Daria Polubenko <daria.polubenko@gmail.com>
2024-10-16 18:39:49 +0300

    update UpdateUserDTO.java

--- app/src/main/java/hexlet/code/dto/UpdateUserDTO.java
+++ app/src/main/java/hexlet/code/dto/UpdateUserDTO.java
@@ -1,11 +1,18 @@
 package hexlet.code.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import org.openapitools.jackson.nullable.JsonNullable;
 
 @Getter
 @Setter
 public class UpdateUserDTO {
-    private String email;
-    private String password;
+    @Email
+    private JsonNullable<String> email;
+
+    @Size(min = 3)
+    private JsonNullable<String> passwordDigest;
 }
