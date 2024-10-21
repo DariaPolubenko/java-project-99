af110c8ae6c5d04af8cd14edfd707abd44c7dcbe
Daria Polubenko <daria.polubenko@gmail.com>
2024-10-21 17:17:49 +0300

    update UserMapper.java

--- app/src/main/java/hexlet/code/mapper/UserMapper.java
+++ app/src/main/java/hexlet/code/mapper/UserMapper.java
@@ -5,11 +5,9 @@ import hexlet.code.dto.CreateUserDTO;
 import hexlet.code.dto.UpdateUserDTO;
 import hexlet.code.dto.UserDTO;
 import hexlet.code.model.User;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingConstants;
-import org.mapstruct.NullValuePropertyMappingStrategy;
-import org.mapstruct.ReportingPolicy;
-import org.mapstruct.MappingTarget;
+import org.mapstruct.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Mapper(
         uses = { JsonNullableMapper.class },
@@ -19,8 +17,22 @@ import org.mapstruct.MappingTarget;
 )
 public abstract class UserMapper {
 
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
+    @Mapping(target = "passwordDigest", source = "password")
     public abstract User map(CreateUserDTO dto);
+
     public abstract UserDTO map(User model);
+
+    @Mapping(target = "passwordDigest", source = "password")
     public abstract void update(UpdateUserDTO dto, @MappingTarget User model);
 
+    public abstract User map(UserDTO model);
+
+    @BeforeMapping
+    public void encryptPassword(CreateUserDTO data) {
+        var password = data.getPassword();
+        data.setPassword(encoder.encode(password));
+    }
 }
