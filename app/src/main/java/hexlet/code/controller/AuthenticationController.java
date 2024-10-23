32529ca5e52de16c4179e64c6cf3c0bd2d253c04
Daria Polubenko <daria.polubenko@gmail.com>
2024-10-24 02:59:32 +0300

    update AuthenticationController.java

--- app/src/main/java/hexlet/code/controller/AuthenticationController.java
+++ app/src/main/java/hexlet/code/controller/AuthenticationController.java
@@ -3,7 +3,6 @@ package hexlet.code.controller;
 import hexlet.code.dto.AuthRequest;
 import hexlet.code.util.JWTUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
