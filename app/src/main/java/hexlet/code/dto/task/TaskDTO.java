16e0f2a394f608aba736ee4ddfb6a8a9e212f674
Daria Polubenko <daria.polubenko@gmail.com>
2024-10-30 19:17:31 +0300

    update TaskDTO.java

--- app/src/main/java/hexlet/code/dto/task/TaskDTO.java
+++ app/src/main/java/hexlet/code/dto/task/TaskDTO.java
@@ -1,6 +1,7 @@
 package hexlet.code.dto.task;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,6 +12,7 @@ import java.time.LocalDate;
 public class TaskDTO {
     private Long id;
     private int index;
+    @JsonProperty("assignee_id")
     private Long assigneeId;
     private String title;
     private String content;
