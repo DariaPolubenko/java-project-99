2fb0d06db29d9ee3e7ddf96e5acb89cb758843d7
Daria Polubenko <daria.polubenko@gmail.com>
2024-11-19 21:15:29 +0300

    update UpdateTaskDTO.java

--- app/src/main/java/hexlet/code/dto/task/UpdateTaskDTO.java
+++ app/src/main/java/hexlet/code/dto/task/UpdateTaskDTO.java
@@ -1,12 +1,16 @@
 package hexlet.code.dto.task;
 
+import hexlet.code.model.Label;
 import lombok.Getter;
 import lombok.Setter;
 import org.openapitools.jackson.nullable.JsonNullable;
+
+import java.util.List;
 
 @Getter
 @Setter
 public class UpdateTaskDTO {
     private JsonNullable<String> title;
     private JsonNullable<String> content;
+    private JsonNullable<List<Long>> labelIds;
 }
