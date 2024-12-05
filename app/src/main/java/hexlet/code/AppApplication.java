71f12c72f5d1a5546cbfe28add523d7b1e270945
Daria Polubenko <daria.polubenko@gmail.com>
2024-12-05 15:39:05 +0300

    update AppApplication.java

--- app/src/main/java/hexlet/code/AppApplication.java
+++ app/src/main/java/hexlet/code/AppApplication.java
@@ -1,13 +1,22 @@
 package hexlet.code;
 
+import io.sentry.Sentry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import io.sentry.spring.jakarta.EnableSentry;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableSentry(dsn = "https://9b8f2f5dbcbc45075cb07dd618f7024a@o4508415448383488.ingest.de.sentry.io/4508415461163088")
 public class AppApplication {
 	public static void main(String[] args) {
+		try {
+			throw new Exception("This is a test.");
+		} catch (Exception e) {
+			Sentry.captureException(e);
+		}
+
 		SpringApplication.run(AppApplication.class, args);
 	}
 }
