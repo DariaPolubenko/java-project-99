dd5a221ab0a18c836f1f5eb98709959f93dbd8b8
Daria Polubenko <daria.polubenko@gmail.com>
2024-12-10 16:05:10 +0300

    update Task.java

--- app/src/main/java/hexlet/code/model/Task.java
+++ app/src/main/java/hexlet/code/model/Task.java
@@ -10,7 +10,6 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
 import java.util.LinkedHashSet;
-import java.util.List;
 import java.util.Set;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
