755ce3cbb58d27dac149d88fb57ecc0f2394cce3
Daria Polubenko <daria.polubenko@gmail.com>
2024-12-10 16:06:07 +0300

    update UpdateTaskDTO.java

--- app/src/main/java/hexlet/code/dto/task/UpdateTaskDTO.java
+++ app/src/main/java/hexlet/code/dto/task/UpdateTaskDTO.java
@@ -1,11 +1,8 @@
 package hexlet.code.dto.task;
 
-import hexlet.code.model.Label;
 import lombok.Getter;
 import lombok.Setter;
 import org.openapitools.jackson.nullable.JsonNullable;
-
-import java.util.List;
 import java.util.Set;
 
 @Getter
