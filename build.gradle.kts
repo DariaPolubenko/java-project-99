0710630d6037ff72070ccb3640b22772627c5f3f
Daria Polubenko <daria.polubenko@gmail.com>
2024-12-11 20:49:12 +0300

    update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
+import org.gradle.internal.impldep.org.fusesource.jansi.AnsiRenderer.test
 
 plugins {
 	java
